1ea389ab7b678bc636f63cb4815b8f23eacf1db0
xasan <xasandj2002@gmail.com>
2022-07-30 15:49:28 +0300

    footer component

---
+++ layouts/Footer/Footer.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { FooterWrapper } from "./footerStyled";
+
+const Footer = () => {
+  return (
+    <>
+      <FooterWrapper>Footer</FooterWrapper>
+    </>
+  );
+};
+
+export default Footer;
