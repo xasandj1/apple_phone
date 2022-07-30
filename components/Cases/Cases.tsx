b772627be0930933a451bba54409ddc204c74197
xasan <xasandj2002@gmail.com>
2022-07-30 15:56:00 +0300

    new components

---
+++ components/Cases/Cases.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { CasesWrapper } from "./casesStyled";
+
+const Cases = () => {
+  return (
+    <>
+      <CasesWrapper>Cases</CasesWrapper>
+    </>
+  );
+};
+
+export default Cases;
---
+++ components/Header/Header.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { HeaderWrapper } from "./headerStyled";
+
+const Header = () => {
+  return (
+    <>
+      <HeaderWrapper>Header</HeaderWrapper>
+    </>
+  );
+};
+
+export default Header;
